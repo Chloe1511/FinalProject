b1d34ec317e5ac5fe6365de8e2c3cf4089abfa8f
Chloe1511 <60323204+Chloe1511@users.noreply.github.com>
2021-03-21 22:45:41 +0200

    p

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ from flask_migrate import Migrate, MigrateCommand, Manager
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key= 'ldfjsolasfuasdfjsodfusoij4w09r8pswojufsldkfjdf9'
 app.config[
     'SQLALCHEMY_DATABASE_URI'] = 'postgres://qigueenywepswf:6dfe096b778783f1d5932be9516b36a87c14cf8cb007a8dca4546f47060021c3@ec2-3-211-37-117.compute-1.amazonaws.com:5432/d5193g7acku5on'
-app.secret_key= 'ldfjsolasfuasdfjsodfusoij4w09r8pswojufsldkfjdf9'
 db = SQLAlchemy(app)
 
 class users(db.Model):
